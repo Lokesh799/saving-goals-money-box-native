9bd4199b800c1976d535447f8b6c01df40951649
developer <jitendralodhi108@gmail.com>
2023-01-17 23:40:40 +0530

    Added homescreen and renamed files

--- App.js
+++ App.js
@@ -1,60 +1,25 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
+// Importing components and screen
 import ContributionList from './app/components/ContributionList';
-import CreatGoals from './app/Screens/CreateGoals';
-// import { createStackNavigator } from '@react-navigation/native-stack';
-// import NavigationContainer from 'react-native-navigation-container';
-// import 'react-native-gesture-handler'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-            const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+import CreatGoals from './app/screens/CreateGoals';
+import MainScreen from './app/screens/MainScreen';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  // const Stack = createStackNavigator(); 
-  // const Stack = createNativeStackNavigator();
-  
-const Tab = createBottomTabNavigator();  
+// importing modules and library
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+
+  const Tab = createBottomTabNavigator();
   return (
-    <NavigationContainer>
-    <Tab.Navigator>
-    <Tab.Screen name="Settings" component={CreatGoals} />
-    <Tab.Screen name="Home" component={ContributionList} />
-  </Tab.Navigator>
-  </NavigationContainer>      
+    <NavigationContainer >
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="MainScreen" component={MainScreen}  />
+        <Tab.Screen name="Settings" component={CreatGoals} />
+        <Tab.Screen name="Home" component={ContributionList} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
---
+++ app/components/AddGoalBtn.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const AddGoalsBtn = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.view1}>
+                <Text style={styles.headertxt}> Goals </Text>
+            </View>
+
+            <View style={styles.view2} >
+                <TouchableOpacity >
+                    <Text style={styles.plusbtn} onPress={() => { props.navigation.navigate('AddGoals') }} > + </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#0d0d0d',
+        flexDirection: 'row',
+    },
+    view1: {
+        width: 250,
+    },
+    view2: {
+        textAlign: 'right',
+        width: 170,
+    },
+    headertxt: {
+        color: 'white',
+        fontSize: 30,
+        paddingTop: 5,
+        textAlign: 'right'
+
+    },
+    plusbtn: {
+        color: 'white',
+        fontSize: 42,
+        paddingRight: 15,
+        marginBottom: 6,
+        textAlign: 'right'
+    }
+})
+export default AddGoalsBtn;
